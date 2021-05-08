b87133aeee43307d0675e6917f43c3657a4bd881
ArildoJunior <arildoajunior@gmail.com>
2021-05-08 08:43:24 -0300

    inicio da API

---
+++ heroessapi/src/test/java/com/DigitalInnovationOne/heroessapi/HeroessapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalInnovationOne.heroessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroessapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
