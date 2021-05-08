13003181dff50acd6d0625ef27ea102fd2b934c0
ArildoJunior <arildoajunior@gmail.com>
2021-05-08 16:03:17 -0300

    test

--- heroesapi/src/main/java/com/Dio/heroesapi/HeroesData.java
+++ heroesapi/src/main/java/com/Dio/heroesapi/HeroesData.java
@@ -18,13 +18,26 @@ public class HeroesData {
 
         Table table = dynamoDB.getTable("Heroes_Api_Table");
 
-        Item item = new Item()
-                .withPrimaryKey("id", "1")
+        Item hero = new Item()
+                .withPrimaryKey("id", "2")
                 .withString("name", "Mulher Maravilha")
                 .withString("universe", "dc comics")
                 .withNumber("films", 2);
 
-        PutItemOutcome outcome = table.putItem(item);
+        Item hero2 = new Item()
+                .withPrimaryKey("id", "3")
+                .withString("name", "Viuva negra")
+                .withString("universe", "marvel")
+                .withNumber("films", 2);
 
+        Item hero3 = new Item()
+                .withPrimaryKey("id", "4")
+                .withString("name", "Capita marvel")
+                .withString("universe", "marvel")
+                .withNumber("films", 2);
+
+        PutItemOutcome outcome1 = table.putItem(hero);
+        PutItemOutcome outcome2 = table.putItem(hero2);
+        PutItemOutcome outcome3 = table.putItem(hero3);
     }
 }
--- heroesapi/src/test/java/com/Dio/heroesapi/HeroesapiApplicationTests.java
+++ heroesapi/src/test/java/com/Dio/heroesapi/HeroesapiApplicationTests.java
@@ -1,13 +1,51 @@
 package com.Dio.heroesapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import repository.HeroesRepository;
+import java.util.List;
+import static constans.HeroesConstant.HEROES_ENDPOINT_LOCAL;
 
-@SpringBootTest
-class HeroesapiApplicationTests {
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+public class HeroesapiApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
+
+	@Autowired
+	HeroesRepository heroesRepository;
 
 	@Test
-	void contextLoads() {
-	}
+	public void getOneHeroeById(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"10")
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody();
+		}
 
-}
+		@Test
+		public void getOneHeronotFound(){
+			webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"10")
+				.exchange()
+				.expectStatus().isNotFound();
+		}
+
+		@Test
+		public void deleteHero(){
+			webTestClient.delete().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"1")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody(Void.class);
+		}
+	}
