1d1aea2a09e8d693ef9a221b97543de2bf49d36e
ArildoJunior <arildoajunior@gmail.com>
2021-05-08 13:50:45 -0300

    Tabela e Dados

--- heroesapi/src/main/java/com/Dio/heroesapi/DynamoConfig.java
+++ heroesapi/src/main/java/com/Dio/heroesapi/DynamoConfig.java
@@ -1,4 +1,4 @@
-package com.Dio.heroesapiconfig;
+package com.Dio.heroesapi;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -8,7 +8,6 @@ import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRep
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.SpringUtil;
 import org.springframework.util.StringUtils;
 
 @Configuration
@@ -35,7 +34,6 @@ public class DynamoConfig {
     @Bean
     public AWSCredentials amazonAWSCredentials(){
         return new BasicAWSCredentials(
-                amazonAWSAcesskey,amazonAWSSecretkey
-        )
+                amazonAWSAcesskey,amazonAWSSecretkey );
     }
 }
